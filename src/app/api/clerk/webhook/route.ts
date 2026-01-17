c119fead2254de6e5d4b23e68221c4b9d8957e61
Antony-Sewe <antonywing6@gmail.com>
2026-01-17 23:26:43 +0300

    add chaining

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -2,7 +2,9 @@ import { db } from "@/server/db";
 
 export const POST = async (req: Request) => {
     const { data } = await req.json();
-    const emailAddress = data.email_addresses[0].email_address;
+
+    //Optional chaining to prevent crush then Fallback to an empty string or null if data not found
+    const emailAddress = data.email_addresses?.[0]?.email_address ?? "";
     const firstName = data.first_name;
     const lastName = data.last_name;
     const imageUrl = data.image_url;
